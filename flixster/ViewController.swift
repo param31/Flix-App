945df37b7b915566cecb59539ddd03e4c6a28e0b
Parampreet Singh <49701247+param31@users.noreply.github.com>
2020-09-24 04:21:36 +0530

    updates

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Parampreet Singh on 9/24/20.
+//  Copyright © 2020 Parampreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
